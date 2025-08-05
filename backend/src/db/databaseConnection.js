a470802294eaec14370af8430ac1ab8d274ec8b2
Parth Bhayani <bhayaniparth932@gmail.com>
2025-08-05 10:50:32 +0530

    first commit

---
+++ backend/src/db/databaseConnection.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const DB_Name = "mern-blog";
+const connectDB = async() => {
+    try
+    {
+      const connect= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_Name}`)
+      console.log("mongodb connected successfully")  
+    } catch (error) 
+    {
+       console.log("MongoDb error",error);
+    }
+}
+
+export default connectDB;
