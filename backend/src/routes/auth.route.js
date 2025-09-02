6be7dcbe1a604da047e984a7dbb065a2a88ad466
Parth Bhayani <bhayaniparth932@gmail.com>
2025-09-02 15:20:35 +0530

    Backend Pushed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,19 +1,3 @@
-// import express from 'express'
-// import { register, signIn, signOut } from '../controllers/auth.controller.js';
-// import {upload} from '../middleware/multer.middleware.js'
-// const authRouter = express.Router();
-
-// authRouter.route("/register").post(
-//     upload.single("userImage"),
-//     register);
-// authRouter.route("/signin").post(signIn)
-// authRouter.route("/signout").post(signOut)
-
-
-// export default authRouter;
-
-
-
 import express from 'express';
 import { register, signIn, signOut } from '../controllers/auth.controller.js';
 import { upload } from '../middleware/multer.middleware.js';
--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,29 +1,29 @@
 import fs from "fs";
 import { v2 as cloudinary } from 'cloudinary'
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret:process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uploadOnCloudinary = async(localFilePath)=>{
+const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
-        const response=await cloudinary.uploader.upload(localFilePath,{
-            resource_type : "auto"
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
         });
 
-        console.log("File uploaded Successfully",response.url)
-        if(fs.existsSync(localFilePath)){
+        console.log("File uploaded Successfully", response.url)
+        if (fs.existsSync(localFilePath)) {
             fs.rmSync(localFilePath)
         }
         return response;
     } catch (error) {
-      console.log("Error occur while uploading on cloudinary",error);
-      if(fs.existsSync(localFilePath)){
+        console.log("Error occur while uploading on cloudinary", error);
+        if (fs.existsSync(localFilePath)) {
             fs.rmSync(localFilePath)
-        } 
-        return null; 
+        }
+        return null;
     }
 };
-export {uploadOnCloudinary};
+export { uploadOnCloudinary };
